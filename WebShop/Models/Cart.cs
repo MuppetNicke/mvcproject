497a9a22217e111ab91c956118403f8046cd858f
Oskar K <fluffest@outlook.com>
2016-04-05 10:18:37 +0200

    Addet data to cart-model

--- WebShop/Models/Cart.cs
+++ WebShop/Models/Cart.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebShop.Models;
 
 namespace WebShop.Models
 {
     class Cart
     {
+        public List<Product> ProductList;
+
+        public Cart()
+        {
+            ProductList = new List<Product>();
+        }
     }
 }
