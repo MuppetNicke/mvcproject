2535f111a2128fc7f2d71fde2a9d565df35a9192
Oskar K <fluffest@outlook.com>
2016-04-05 10:00:03 +0200

    Added model for User

---
+++ WebShop/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Models
+{
+    class User
+    {
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public User(string pName, string pEmail, string pPassword)
+        {
+            Name = pName;
+            Email = pEmail;
+            Password = pPassword;
+        }
+    }
+}
