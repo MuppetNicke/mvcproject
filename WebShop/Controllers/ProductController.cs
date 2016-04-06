9b2cd5b5e98083e82c907a3c8696ad06ebd3693d
Oskar K <fluffest@outlook.com>
2016-04-06 10:06:30 +0200

    Fixed admin posibility to add a product

---
+++ WebShop/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebShop.Models;
+
+namespace WebShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpPost]
+        public ActionResult AddProduct()
+        {
+
+            string tmpName = Request["productName"];
+            int tmpPrice, tmpStock;
+
+            if(int.TryParse(Request["productPrice"], out tmpPrice) && int.TryParse(Request["productStock"], out tmpStock)
+                && tmpName != null)
+            {
+                List<Product> tmpList = (List<Product>)Session["ListOfProducts"];
+                tmpList.Add(new Product ( tmpName, tmpPrice, tmpStock ));
+            }
+
+            return Redirect("/Default/Index");
+        }
+    }
+}
