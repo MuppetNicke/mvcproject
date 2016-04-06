c935d16bd4c87de37d9794f421e5ebbf5b2df64d
Rickard W <rickard.wiesner@hotmail.com>
2016-04-06 12:22:17 +0200

    Added search function moved GetProduct() to ProductController

--- WebShop/Controllers/ProductController.cs
+++ WebShop/Controllers/ProductController.cs
@@ -16,11 +16,44 @@ namespace WebShop.Controllers
             string tmpName = Request["productName"];
             int tmpPrice, tmpStock;
 
-            if(int.TryParse(Request["productPrice"], out tmpPrice) && int.TryParse(Request["productStock"], out tmpStock)
+            if (int.TryParse(Request["productPrice"], out tmpPrice) && int.TryParse(Request["productStock"], out tmpStock)
                 && tmpName != null)
             {
                 List<Product> tmpList = (List<Product>)Session["ListOfProducts"];
-                tmpList.Add(new Product ( tmpName, tmpPrice, tmpStock ));
+                tmpList.Add(new Product(tmpName, tmpPrice, tmpStock));
+            }
+
+            return Redirect("/Default/Index");
+        }
+
+        [HttpGet]
+        public ActionResult GetProduct(int id)
+        {
+            List<Product> tmpList = (List<Product>)Session["ListOfProducts"];
+            Product tmpProduct = tmpList[id];
+
+            return View(tmpProduct);
+        }
+
+        [HttpPost]
+        public ActionResult SearchForProduct()
+        {
+            int id = 0;
+            string prodSearch = Request["searchField"];
+            int productId = 0;
+
+            List<Product> tmpList = (List<Product>)Session["ListOfProducts"];
+            foreach (var product in tmpList)
+            {
+
+                if (prodSearch == product.Name)
+                {
+                    productId = id;
+
+                    return RedirectToAction("GetProduct/" + id);
+                }
+
+                id++;
             }
 
             return Redirect("/Default/Index");
